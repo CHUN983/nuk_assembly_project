c3b6e83380b3524e630d30b15a42cc3cfa31faf7
sunthing <apple0908070605@gmail.com>
2024-06-03 22:33:21 +0800

    game finish inistall
    the game can play but boring

--- assembly_project/library/game_func.asm
+++ assembly_project/library/game_func.asm
@@ -1,4 +1,4 @@
-INCLUDE mylib.inc
+INCLUDE Irvine32.inc
 
 .data
 
@@ -61,5 +61,179 @@ UPDATE_PLAYER PROC
 	ret
 UPDATE_PLAYER ENDP
 
+
+;------------------------------------------------------------------
+;讓ball按照ebx上的state移動，移動完後看是否超出去，超過esi設定為1
+;receives: eax, ebx, edx, esi, ebp
+;return: edx, esi, [epb+12](for path)
+;require: edx的值必須是ball的位置!
+BALL_MOVE PROC
+;---------------------------------------------------------------------
+	push eax
+	push ebp
+	mov ebp, esp
+
+    cmp eax, 1
+    je EAX_1
+    cmp eax, 2
+    je EAX_2
+    cmp eax, 3
+    je EAX_3
+    cmp eax, 4
+    je EAX_4
+    cmp eax, 5
+    je EAX_5
+    cmp eax, 6
+    je EAX_6
+    cmp eax, 7
+    je EAX_7
+    cmp eax, 8
+    je EAX_8
+    cmp eax, 9
+    je EAX_9
+    cmp eax, 10
+    je EAX_10
+    cmp eax, 11
+    je EAX_11
+    cmp eax, 12
+    je EAX_12
+    cmp eax, 13
+    je EAX_13
+    cmp eax, 14
+    je EAX_14
+    jmp STOP
+
+	EAX_1:    ; 上上
+		add dh, 2
+		jmp STOP
+
+	EAX_2:    ; 上上右
+		add dh, 2
+		add dl, 1
+		jmp STOP
+
+	EAX_3:    ; 上右
+		add dh, 1
+		add dl, 1
+		jmp STOP
+
+	EAX_4:    ; 上右右
+		add dh, 1
+		add dl, 2
+		jmp STOP
+
+	EAX_5:    ; 上上左
+		add dh, 2
+		sub dl, 1
+		jmp STOP
+
+	EAX_6:    ; 上左
+		add dh, 1
+		sub dl, 1
+		jmp STOP
+
+	EAX_7:    ; 上左左
+		add dh, 1
+		sub dl, 2
+		jmp STOP
+
+	EAX_8:    ; 下下
+		sub dh, 2
+		jmp STOP
+
+	EAX_9:    ; 下下右
+		sub dh, 2
+		add dl, 1
+		jmp STOP
+
+	EAX_10:   ; 下右
+		sub dh, 1
+		add dl, 1
+		jmp STOP
+
+	EAX_11:   ; 下右右
+		sub dh, 1
+		add dl, 2
+		jmp STOP
+
+	EAX_12:   ; 下下左
+		sub dh, 2
+		sub dl, 1
+		jmp STOP
+
+	EAX_13:   ; 下左
+		sub dh, 1
+		sub dl, 1
+		jmp STOP
+
+	EAX_14:   ; 下左左
+		sub dh, 1
+		sub dl, 2
+		jmp STOP
+
+	;需要計算兩個，超出邊界或碰到player
+	STOP:
+		player:
+		;計算是否碰到player
+			.IF dh >= 28
+				mov ebx, [ebp+16]
+				and ebx, 00FFh
+				.IF dl >= bl 
+					add bl, 4
+					.IF dl <= bl
+						mov dh, 27
+						mov esi, 1
+						mov WORD PTR [ebp+12], 0
+					.ENDIF
+				.ENDIF
+			.ENDIF
+
+			.IF dh <= 6
+				mov ebx, [ebp+16]
+				and ebx, 00FFh
+				.IF dl >= bl
+					add bl, 4
+					.IF dl <= bl
+						mov dh, 7
+						mov esi, 1
+						mov WORD PTR [ebp+12], 0
+					.ENDIF
+				.ENDIF
+			.ENDIF
+
+		;計算是否超出邊界
+		border:
+			;邊界上的判斷
+			.IF dh >= 29
+				.IF dl<=42 || dl>=63
+					mov dh, 28
+					mov esi, 1
+					mov WORD PTR [ebp+12], 0 ; 修改path
+				.ENDIF
+			.ENDIF
+			.IF dh <= 5
+				.IF dl<=42 || dl >=63
+					mov dh, 6
+					mov esi, 1
+					mov WORD PTR [ebp+12], 0
+				.ENDIF
+			.ENDIF
+			.IF dl >= 5+75
+				mov dl, 5+74
+				mov esi, 1
+				mov WORD PTR [ebp+12], 1
+			.ENDIF
+			.IF dl <= 20
+				mov dl, 21
+				mov esi,1
+				mov WORD PTR [ebp+12], 1
+			.ENDIF
+
+		continue:
+	pop ebp
+	pop eax
+
+	ret
+BALL_MOVE ENDP
 
 END
--- assembly_project/main/main/main.asm
+++ assembly_project/main/main/main.asm
@@ -43,28 +43,43 @@ VK_SPACEBAR	EQU		000000020h
 	GAME_RULE BYTE 'Rule：',0ah,0dh
 			  BYTE "The player1 is on the buttom, keyword a and d represent left and right respectively.",0ah, 0dh
 			  BYTE "The player2 is on the top, keyword j and l represent left and right respectively.",0,0ah, 0dh
-	GAME_GROUND BYTE '=====================                              =========================',0,0ah, 0dh
+	GAME_GROUND BYTE '===================                    ===================',0,0ah, 0dh
 	GAME_SIDE_GROUND BYTE'||',0,0ah,0dh
 
-	GAME_MID_GROUND BYTE  '---------------------------------------------------------------------------',0,0ah, 0dh
+	GAME_MID_GROUND BYTE  '-----------------------------------------------------------',0,0ah, 0dh
 
 	inputChar BYTE ?
 
-	;ball pos
-	xPos_ball BYTE 35
-	yPos_ball BYTE 17
-
 	player BYTE "*****",0
 	;player1 pos
-	xPos_player1 BYTE 35
+	xPos_player1 BYTE 48
 	yPos_player1 BYTE 28
 	;player2 pos
-	xPos_player2 BYTE 35
+	xPos_player2 BYTE 48
 	yPos_player2 BYTE 6
 
 	;信號，決定哪個可以動
 	semaphore BYTE 0
+	;時間戳記，每經過20次的信號處理需要處理一此ball
+	tamp WORD 20
 
+
+	;球所需要的data----------------------------------------------------------------------------
+
+	;ball pos
+	xPos_ball BYTE 50
+	yPos_ball BYTE 17
+
+	state DWORD 10
+	hit_wall DWORD 0
+	path DWORD 0    ;板子正在右移，左移，還是靜止 
+									;橫向靜止=0 同時改變上下左右
+									;直向靜止=1 只會改變左右
+									;右移=2     改變上下左右的同時改變速度
+									;左移=3     改變上下左右的同時改變速度
+	;-------------------------------------------------------------------------------------------
+
+	word_test Dword 0
 .code
 main PROC
 
@@ -94,21 +109,37 @@ main PROC
 		.IF ah
 			jmp exitGame
 		.ENDIF
+
 		mov eax,3
 		call delay
+
+
+		.IF tamp==0
+			mov tamp, 5
+			mov dl, xPos_ball
+			mov dh, yPos_ball
+			call UPDATE_BALL
+
+			;當ball沒被接住時結束遊戲
+			.IF yPos_ball<6 || yPos_ball > 28
+				.IF xPos_ball > 42 && xPos_ball < 62
+					jmp exitGame
+				.ENDIF
+			.ENDIF
+		.ENDIF
+
 		;依順序決定player1或player2，類似做context switch的概念
 		not semaphore
 		
 		;get user input
 		.IF semaphore==0
+			dec tamp
 
 			;for player1
 			mov ah, 0
 			INVOKE GetKeyState, VK_A
 			.IF ah
 				mov inputChar,"a"
-				;mov eax,3
-				;call delay
 				mov al, "a"
 				jmp moveLeft
 
@@ -117,8 +148,6 @@ main PROC
 			INVOKE GetKeyState, VK_D
 			.IF ah
 				mov inputChar,"d"
-				;mov eax,3
-				;call delay
 				mov al, "d"
 				jmp moveRight
 			.ENDIF
@@ -126,6 +155,7 @@ main PROC
 		.ENDIF
 
 		.IF semaphore == 0FFh
+			dec tamp
 
 		;for player2
 			mov ah, 0
@@ -148,8 +178,9 @@ main PROC
 				jmp moveright
 			.ENDIF
 
-
 		.ENDIF
+
+
 
 		jmp gameloop ;防止白癡亂按其他按鈕
 
@@ -161,7 +192,7 @@ main PROC
 
 			player1_time_left:
 				;先看看有沒有超過邊界，有的話直接跳
-				cmp xPos_player1, 2
+				cmp xPos_player1, 22
 				jle stop
 
 				mov dl, xPos_player1
@@ -175,7 +206,7 @@ main PROC
 
 			player2_time_left:
 				;先看看有沒有超過邊界，有的話直接跳
-				cmp xPos_player2, 2
+				cmp xPos_player2, 22
 				jle stop
 
 				mov dl, xPos_player2
@@ -197,7 +228,7 @@ main PROC
 			;執行各自player的位置(往右一格)
 			player1_time_right:
 				;先看看有沒有超過邊界，有的話直接跳
-				cmp xPos_player1, SIZEOF GAME_GROUND-7
+				cmp xPos_player1, SIZEOF GAME_GROUND+14
 				jge stop
 
 				mov dl, xPos_player1
@@ -210,7 +241,7 @@ main PROC
 
 			player2_time_right:
 				;先看看有沒有超過邊界，有的話直接跳
-				cmp xPos_player2, SIZEOF GAME_GROUND-7
+				cmp xPos_player2, SIZEOF GAME_GROUND+14
 				jge stop
 
 				mov dl, xPos_player2
@@ -248,19 +279,19 @@ GROUND PROC
 	call WriteString
 	
 	;draw ground button and top (height:0~24, width: 1~?)
-	mov dl, 2
+	mov dl, 22
 	mov dh, 5
 	call Gotoxy
 	mov edx, OFFSET GAME_GROUND
 	CALL WriteString
-	MOV dl, 2
+	MOV dl, 22
 	mov dh, 29
 	call GOtoxy
 	mov edx,OFFSET GAME_GROUND
 	call WriteString
 
 	;draw mid
-	mov dl, 2
+	mov dl, 22
 	mov	dh, 17
 	call Gotoxy
 	mov edx, OFFSET GAME_MID_GROUND
@@ -271,7 +302,7 @@ GROUND PROC
 	mov bl, 5
 	side1:
 		mov dh, bl
-		mov dl, 0
+		mov dl, 20
 		call Gotoxy
 		inc bl
 		
@@ -284,7 +315,7 @@ GROUND PROC
 	mov bl,5
 	side2:
 		mov dh, bl
-		mov dl, SIZEOF GAME_MID_GROUND
+		mov dl, SIZEOF GAME_SIDE_GROUND+75
 		call Gotoxy
 		inc bl
 
@@ -312,6 +343,381 @@ GROUND PROC
 GROUND ENDP
 
 
+UPDATE_BALL PROC
+	
+	push eax
+
+	;假設dl已經在目前球的位置，先清除為" "或"-"
+	call Gotoxy
+	mov al," "
+	.IF dh==17
+		mov al, "-"
+	.ENDIF
+	.IF dh== 29 || dh == 5
+		mov al, "="
+	.ENDIF
+	call WriteChar
+
+	;往既定的方向移動,eax儲存state，並判斷是否hit
+	mov esi, 0
+	mov eax, state
+	mov dl, xPos_ball
+	mov dh, yPos_ball
+	.IF state > 0 && state < 8
+		push DWORD PTR xPos_player1
+	.ENDIF
+	.IF state < 15 && state > 7
+		push DWORD PTR xPos_player2
+	.ENDIF
+
+	push path
+	call BALL_MOVE
+	pop path
+	;xPos_player沒變，直接把esp一回去就好，不用再是player1還是player2
+	add esp, 4
+
+	mov hit_wall,esi
+	mov xPos_ball, dl
+	mov yPos_ball, dh
+
+
+	;沒有hit的話照既定方向移動
+	call Gotoxy
+	mov ax, "@"
+	call WriteChar
+
+	;有hit的話先改變運動方向，然後移動
+	.IF hit_wall==1
+		;改state的值，目的修改球的移動方向，最後存回path
+		call BALL_STATE		;notion上面一段的
+		call BALL_MOVE
+
+	.ENDIF
+
+	pop eax
+	ret
+UPDATE_BALL ENDP
+
+BALL_STATE PROC
+
+	;判斷板子往右還是往左還是沒動
+	mov eax, path 
+	cmp eax, 0
+	je PATH_0
+	cmp eax, 1
+	je PATH_1
+	cmp eax, 2
+	je PATH_2
+	cmp eax, 3
+	je PATH_3
+	jmp CONTINUE
+
+	PATH_0:    ; 如果是?向靜止
+		mov eax, state
+		cmp eax, 1
+		je STATE_1_0
+		cmp eax, 2
+		je STATE_2_0
+		cmp eax, 3
+		je STATE_3_0
+		cmp eax, 4
+		je STATE_4_0
+		cmp eax, 5
+		je STATE_5_0
+		cmp eax, 6
+		je STATE_6_0
+		cmp eax, 7
+		je STATE_7_0
+		cmp eax, 8
+		je STATE_8_0
+		cmp eax, 9
+		je STATE_9_0
+		cmp eax, 10
+		je STATE_10_0
+		cmp eax, 11
+		je STATE_11_0
+		cmp eax, 12
+		je STATE_12_0
+		cmp eax, 13
+		je STATE_13_0
+		cmp eax, 14
+		je STATE_14_0
+		jmp CONTINUE
+
+	STATE_1_0:
+		mov eax, 8
+		jmp CONTINUE
+	STATE_2_0:
+		mov eax, 9
+		jmp CONTINUE
+	STATE_3_0:
+		mov eax, 10
+		jmp CONTINUE
+	STATE_4_0:
+		mov eax, 11
+		jmp CONTINUE
+	STATE_5_0:
+		mov eax, 12
+		jmp CONTINUE
+	STATE_6_0:
+		mov eax, 13
+		jmp CONTINUE
+	STATE_7_0:
+		mov eax, 14
+		jmp CONTINUE
+	STATE_8_0:
+		mov eax, 1
+		jmp CONTINUE
+	STATE_9_0:
+		mov eax, 2
+		jmp CONTINUE
+	STATE_10_0:
+		mov eax, 3
+		jmp CONTINUE
+	STATE_11_0:
+		mov eax, 4
+		jmp CONTINUE
+	STATE_12_0:
+		mov eax, 5
+		jmp CONTINUE
+	STATE_13_0:
+		mov eax, 6
+		jmp CONTINUE
+	STATE_14_0:
+		mov eax, 7
+		jmp CONTINUE
+
+	PATH_1:    ; 如果是直向靜止
+		mov eax, state
+		cmp eax, 1
+		je STATE_1_1
+		cmp eax, 2
+		je STATE_2_1
+		cmp eax, 3
+		je STATE_3_1
+		cmp eax, 4
+		je STATE_4_1
+		cmp eax, 5
+		je STATE_5_1
+		cmp eax, 6
+		je STATE_6_1
+		cmp eax, 7
+		je STATE_7_1
+		cmp eax, 8
+		je STATE_8_1
+		cmp eax, 9
+		je STATE_9_1
+		cmp eax, 10
+		je STATE_10_1
+		cmp eax, 11
+		je STATE_11_1
+		cmp eax, 12
+		je STATE_12_1
+		cmp eax, 13
+		je STATE_13_1
+		cmp eax, 14
+		je STATE_14_1
+		jmp CONTINUE
+
+	STATE_1_1:
+		mov eax, 8
+		jmp CONTINUE
+	STATE_2_1:
+		mov eax, 5
+		jmp CONTINUE
+	STATE_3_1:
+		mov eax, 6
+		jmp CONTINUE
+	STATE_4_1:
+		mov eax, 7
+		jmp CONTINUE
+	STATE_5_1:
+		mov eax, 2
+		jmp CONTINUE
+	STATE_6_1:
+		mov eax, 3
+		jmp CONTINUE
+	STATE_7_1:
+		mov eax, 4
+		jmp CONTINUE
+	STATE_8_1:
+		mov eax, 1
+		jmp CONTINUE
+	STATE_9_1:
+		mov eax, 12
+		jmp CONTINUE
+	STATE_10_1:
+		mov eax, 13
+		jmp CONTINUE
+	STATE_11_1:
+		mov eax, 14
+		jmp CONTINUE
+	STATE_12_1:
+		mov eax, 9
+		jmp CONTINUE
+	STATE_13_1:
+		mov eax, 10
+		jmp CONTINUE
+	STATE_14_1:
+		mov eax, 11
+		jmp CONTINUE
+
+	PATH_2:    ; 如果是?向右移
+		mov eax, state
+		cmp eax, 1
+		je STATE_1_2
+		cmp eax, 2
+		je STATE_2_2
+		cmp eax, 3
+		je STATE_3_2
+		cmp eax, 4
+		je STATE_4_2
+		cmp eax, 5
+		je STATE_5_2
+		cmp eax, 6
+		je STATE_6_2
+		cmp eax, 7
+		je STATE_7_2
+		cmp eax, 8
+		je STATE_8_2
+		cmp eax, 9
+		je STATE_9_2
+		cmp eax, 10
+		je STATE_10_2
+		cmp eax, 11
+		je STATE_11_2
+		cmp eax, 12
+		je STATE_12_2
+		cmp eax, 13
+		je STATE_13_2
+		cmp eax, 14
+		je STATE_14_2
+		jmp CONTINUE
+
+	STATE_1_2:
+		mov eax, 9
+		jmp CONTINUE
+	STATE_2_2:
+		mov eax, 8
+		jmp CONTINUE
+	STATE_3_2:
+		mov eax, 12
+		jmp CONTINUE
+	STATE_4_2:
+		mov eax, 13
+		jmp CONTINUE
+	STATE_5_2:
+		mov eax, 10
+		jmp CONTINUE
+	STATE_6_2:
+		mov eax, 11
+		jmp CONTINUE
+	STATE_7_2:
+		mov eax, 11
+		jmp CONTINUE
+	STATE_8_2:
+		mov eax, 2
+		jmp CONTINUE
+	STATE_9_2:
+		mov eax, 1
+		jmp CONTINUE
+	STATE_10_2:
+		mov eax, 5
+		jmp CONTINUE
+	STATE_11_2:
+		mov eax, 6
+		jmp CONTINUE
+	STATE_12_2:
+		mov eax, 3
+		jmp CONTINUE
+	STATE_13_2:
+		mov eax, 4
+		jmp CONTINUE
+	STATE_14_2:
+		mov eax, 4
+		jmp CONTINUE
+
+	PATH_3:    ;如果是?向左移
+		mov eax, state
+		cmp eax, 1
+		je STATE_1_3
+		cmp eax, 2
+		je STATE_2_3
+		cmp eax, 3
+		je STATE_3_3
+		cmp eax, 4
+		je STATE_4_3
+		cmp eax, 5
+		je STATE_5_3
+		cmp eax, 6
+		je STATE_6_3
+		cmp eax, 7
+		je STATE_7_3
+		cmp eax, 8
+		je STATE_8_3
+		cmp eax, 9
+		je STATE_9_3
+		cmp eax, 10
+		je STATE_10_3
+		cmp eax, 11
+		je STATE_11_3
+		cmp eax, 12
+		je STATE_12_3
+		cmp eax, 13
+		je STATE_13_3
+		cmp eax, 14
+		je STATE_14_3
+		jmp CONTINUE
+
+	STATE_1_3:
+		mov eax, 12
+		jmp CONTINUE
+	STATE_2_3:
+		mov eax, 13
+		jmp CONTINUE
+	STATE_3_3:
+		mov eax, 14
+		jmp CONTINUE
+	STATE_4_3:
+		mov eax, 14
+		jmp CONTINUE
+	STATE_5_3:
+		mov eax, 8
+		jmp CONTINUE
+	STATE_6_3:
+		mov eax, 9
+		jmp CONTINUE
+	STATE_7_3:
+		mov eax, 10
+		jmp CONTINUE
+	STATE_8_3:
+		mov eax, 5
+		jmp CONTINUE
+	STATE_9_3:
+		mov eax, 6
+		jmp CONTINUE
+	STATE_10_3:
+		mov eax, 7
+		jmp CONTINUE
+	STATE_11_3:
+		mov eax, 7
+		jmp CONTINUE
+	STATE_12_3:
+		mov eax, 1
+		jmp CONTINUE
+	STATE_13_3:
+		mov eax, 2
+		jmp CONTINUE
+	STATE_14_3:
+		mov eax, 3
+		jmp CONTINUE
+
+	CONTINUE:
+		mov state, eax
+
+
+ret
+BALL_STATE ENDP
 
 
 END main	
