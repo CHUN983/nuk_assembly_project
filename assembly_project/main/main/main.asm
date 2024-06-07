c58c1b7eeccf8d489c7e1dd8cd26e44bb45fdf6b
SiroKu_ <56777898+SiroKu1006@users.noreply.github.com>
2024-06-07 14:24:04 +0800

    END

--- assembly_project/main/main/main.asm
+++ assembly_project/main/main/main.asm
@@ -87,9 +87,9 @@ VK_SPACEBAR	EQU		000000020h
 	GAME_END_P1 BYTE '  ',0ah,0dh
 	BYTE   '                =====================================================',0ah,0dh
 	BYTE   '               ||                  __                               ||',0ah,0dh
-	BYTE   '               ||                 |  )         /|                   ||',0ah,0dh
+	BYTE   '               ||                 |  )          |                   ||',0ah,0dh
 	BYTE   '               ||                 |￣           |                   ||',0ah,0dh
-	BYTE   '               ||                 |             |                   ||',0ah,0dh
+	BYTE   '               ||                                                   ||',0ah,0dh
 	BYTE   '               ||                       _____                       ||',0ah,0dh
 	BYTE   '               ||           \   ^   /     |     |\  |               ||',0ah,0dh
 	BYTE   '               ||            \ / \ /      |     | \ |               ||',0ah,0dh
@@ -105,10 +105,10 @@ VK_SPACEBAR	EQU		000000020h
 
 	GAME_END_P2 BYTE '  ',0ah,0dh
 	BYTE   '                =====================================================',0ah,0dh
-	BYTE   '               ||                  __                               ||',0ah,0dh
-	BYTE   '               ||                 |  )         /|                   ||',0ah,0dh
-	BYTE   '               ||                 |￣           |                   ||',0ah,0dh
-	BYTE   '               ||                 |             |                   ||',0ah,0dh
+	BYTE   '               ||                  __         __                    ||',0ah,0dh
+	BYTE   '               ||                 |  )        __|                   ||',0ah,0dh
+	BYTE   '               ||                 |￣        |__                    ||',0ah,0dh
+	BYTE   '               ||                                                   ||',0ah,0dh
 	BYTE   '               ||                       _____                       ||',0ah,0dh
 	BYTE   '               ||           \   ^   /     |     |\  |               ||',0ah,0dh
 	BYTE   '               ||            \ / \ /      |     | \ |               ||',0ah,0dh
